02d382c2f54cb44ae0f7989e03f65097023fe2cb
Marcin Łagowski <marcinlagowski92@gmail.com>
2016-06-14 16:41:30 +0200

    Fixed image name.

--- run_master.sh
+++ run_master.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker run -t -P -d --name spark-master turbineanalytics/sdocker-spark-cassandra /start-master.sh
+sudo docker run -t -P -d --name spark-master turbineanalytics/docker-spark-cassandra /start-master.sh
